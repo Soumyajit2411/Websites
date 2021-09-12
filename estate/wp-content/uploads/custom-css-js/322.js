35af25e7e12fb4717761904b3d48407ca27c9ba1
Soumyajit Roy <soumyajitroy2411@gmail.com>
2021-09-13 03:07:21 +0530

    s

--- estate/wp-content/uploads/custom-css-js/322.js
+++
@@ -1,8 +0,0 @@
-<!-- start Simple Custom CSS and JS -->
-<script type="text/javascript">
-/* Default comment here */ 
-
-jQuery(document).ready(function(){
-  jQuery(".elementor-search-form").append('<input type="hidden" name="post_type" value="property">');
-});</script>
-<!-- end Simple Custom CSS and JS -->
